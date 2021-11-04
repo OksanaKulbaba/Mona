27cd6843deb55e73cc47fcf608ce55be11b81547
Yevhen Parkhomenko <yevhen@huckleberry-labs.com>
2021-11-04 19:16:51 +0200

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,120 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+interface RecordData {
+  time: Date;
+  record: string;
 }
 
-interface Food {
-  value: string;
+interface RecordShown {
+  [key: string]: string;
+}
+interface Rec{
+  [key: string]: string[];
+}
+
+interface Intervals {
+  value: number;
   viewValue: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  selectedValue: string ='';
-  mass = ['position', 'name', 'weight', 'symbol','position1', 'name1', 'weight1', 'symbol1']
+export class AppComponent implements OnInit {
+  selectedValue: number = 5;
+  rangeColumns: Array<string> = [];// this.generateColumn()
+  rangeColumnsForColculate: Array<number> = [];
+  dataBase = AppComponent.generateRandomDate();
+  dataSource: RecordShown[] = []
 
-
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+  intervals: Intervals[] = [
+    {value: 5, viewValue: '5 Min'},
+    {value: 30, viewValue: '30 Min'},
+    {value: 60, viewValue: '1 hour'}
   ];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+
+  constructor() {
+    console.log('this.base',this.dataBase)
+  }
+
+  ngOnInit() {
+    this.generateColumn('60')
+    console.log('this.data',this.dataSource)
+  }
+
+  private static generateRandomDate(): Array<RecordData> {
+    const records: Array<RecordData> = []
+    const start: Date = new Date(2021, 10, 1)
+    const end: Date = new Date(2021, 11, 1);
+    for (let i = 0; i < 1000; i++) {
+      const time = AppComponent.randomDate(start, end);
+      records.push({
+        time: time,
+        record: 'Detail: ' + time.toLocaleTimeString().slice(0,5),
+      })
+    }
+    return records;
+  }
+  private static randomDate(start: Date, end: Date): Date {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+
+  public generateColumn($event?: any) {
+    this.selectedValue = $event;
+    let columnsD: Array<string> = ['date'];
+    const range = (24 * 60) / $event
+    for (let i = 1; i <= range; i++) {
+      const columnName = i * $event;
+      columnsD.push(AppComponent.transformDate(columnName))
+      this.rangeColumnsForColculate.push(i * $event)
+    }
+    this.rangeColumns = columnsD;
+    this.dataSource = [];
+    this.getData();
+  }
+
+  private getData(): void {
+    const allDate = this.dataBase.map(d => {
+      return d.time.getDate()
+    }).filter((v, i, a) => a.indexOf(v) === i).sort( (a,b)=> a-b);
+    allDate.forEach(date => {
+      const recordsForDay =  this.getRecordsForDate(date);
+      this.dataSource.push(recordsForDay)
+      }
+    )
+  }
+
+  getRecordsForDate(date: number) {
+    let result: RecordShown= {};
+    const allRecordsByDay = this.dataBase.filter(record => record.time.getDate() === date);
+    this.rangeColumnsForColculate.forEach(range =>{
+       result  = {
+         ... result,
+         [AppComponent.transformDate(range)]: this.getRecordByRange(range, allRecordsByDay)
+       }
+      }
+    )
+    result['date'] = date.toString();
+    return result;
+  }
+
+  getRecordByRange(range: number, recordDate: RecordData[]): string {
+
+    return recordDate.filter(record => {
+      const minute = record.time.getHours() * 60 + record.time.getMinutes();
+      if (minute <= range && minute > (range - this.selectedValue)) {
+        return record;
+      }
+      return;
+    }).map(rec => rec.record).toString() ;
+  }
+  private static transformDate(columnName: number): string{
+    const h = Math.trunc(columnName / 60)
+    const m = columnName % 60
+    return `${ h > 9 ? h : '0' + h }:${ m > 9 ? m : '0' + m }`;
+  }
+
 }
